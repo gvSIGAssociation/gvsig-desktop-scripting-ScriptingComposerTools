02fc8114eadcbf69ee8a309b3c57cb2444430140
fdiaz <fdiaz@gvsig.com>
2024-10-23 09:47:31 +0200

    Añadido panel para ver el código fuente en archivos HTML

--- editors/html/editor.py
+++ editors/html/editor.py
@@ -5,22 +5,25 @@ from gvsig import logger, LOGGER_WARN
 
 from StringIO import StringIO
 
-from org.gvsig.scripting.swing.impl.composer.editors import ExternalFileEditor
+from org.gvsig.scripting.swing.impl.composer.editors import TextEditor
 
 from org.gvsig.tools.swing.api import ToolsSwingLocator
 
-class HTMLViewer(ExternalFileEditor):
+class HTMLViewer(TextEditor):
   def __init__(self, unit):
-    ExternalFileEditor.__init__(self)
+    self.__browser = None
+    TextEditor.__init__(self, unit)
     self.__browser = ToolsSwingLocator.getToolsSwingManager().createJWebBrowser()
     self.addPanel("Viewer",self.__browser.asJComponent())
     self.set(unit)
 
   def set(self, unit):
-    ExternalFileEditor.set(self, unit)
+    TextEditor.set(self, unit)
+    if self.__browser == None:
+      return
     self.__browser.location(unit.getExternalFile().toURI().toURL())
     try:
-      self.getTabbedPane().setSelectedIndex(1)
+      self.getTabbedPane().setSelectedIndex(2)
     except:
       pass
 
